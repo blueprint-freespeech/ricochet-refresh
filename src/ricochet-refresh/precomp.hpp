b4e268d4a246aa504daa057d84d0ba04f1ce874d
Morgan <morgan@torproject.org>
2025-10-19 10:12:17 +0000

    libtego_ui, ricochet-refresh: combined source into ricochet-refresh target

--- src/ricochet-refresh/precomp.hpp
+++ src/ricochet-refresh/precomp.hpp
@@ -1,10 +1,29 @@
 // pull in Q_OS_ defines
 #include <QtGlobal>
 
+// C standard library
+#include <limits.h>
+
+// C++ standard library
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <type_traits>
+#include <cstdint>
+#include <functional>
+#include <fstream>
+#include <iterator>
+#include <set>
+#include <random>
+
 // Windows
 #if defined(Q_OS_WIN)
 #   include <windows.h>
 #endif
+
+// fmt
+#include <fmt/format.h>
+#include <fmt/ostream.h>
 
 // Qt
 #if defined(Q_OS_WIN)
@@ -12,20 +31,32 @@
 #endif
 #include <QApplication>
 #include <QByteArray>
+#include <QClipboard>
+#include <QDateTime>
 #include <QDir>
 #include <QFile>
+#include <QFileDialog>
+#include <QGuiApplication>
 #include <QHostAddress>
 #include <QIcon>
 #include <QJsonArray>
 #include <QJsonObject>
 #include <QLibraryInfo>
 #include <QLockFile>
+#ifdef Q_OS_MAC
+#   include <QtMac>
+#endif // Q_OS_MAC
 #include <QMessageBox>
 #include <QObject>
+#include <QQuickItem>
+#include <QRandomGenerator>
+#include <QRegularExpression>
+#include <QRegularExpressionValidator>
+#include <QScreen>
 #include <QSettings>
 #include <QStandardPaths>
+#include <QtQml>
 #include <QTranslator>
-#include <QRandomGenerator>
 
 // tego
 #include <tego/tego.hpp>
